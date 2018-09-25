db97bb26cf1befab15238ffb6aeb894420cb2455
ssmith180 <scs@exitsinc.com>
2018-09-24 21:36:16 -0500

    Basic chat possible

---
+++ Assets/ChatController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class ChatController : MonoBehaviour {
+
+    private NetworkManager myNetworkManager;
+
+    private int randomId;
+    private int messageSendCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        print("MyNetworkBehaviour.Start");
+        myNetworkManager = GameObject.FindGameObjectWithTag("NetworkController").GetComponent<NetworkManager>();
+        randomId = Random.Range(0, 100000);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myNetworkManager.client.Send(500, new StringMessage(string.Format("Msg {0} from client {1}", messageSendCount++, randomId)));
+        }
+    }
+}
---
+++ Assets/MyNetworkBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkBehaviour : NetworkBehaviour {
+
+    private NetworkManager myNetworkManager;
+
+	// Use this for initialization
+	void Start () {
+
+        print("MyNetworkBehaviour.Start");
+        myNetworkManager = GameObject.FindGameObjectWithTag("NetworkController").GetComponent<NetworkManager>();
+
+    }
+
+    public override void OnStartClient()
+    {
+        print("MyNetworkBehaviour.OnStartClient");
+        //// Now how do I get my client?
+        //if (GetComponent<NetworkIdentity>().isLocalPlayer)
+        //{
+        //    myNetworkManager.client.RegisterHandler(100, OnHello);
+        //}
+    }
+
+    public void OnHello(NetworkMessage netMsg)
+    {
+        print("Something called hello here in MyNetworkBehaviour");
+    }
+
+}
